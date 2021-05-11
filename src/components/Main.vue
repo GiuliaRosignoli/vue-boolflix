96fa8065956d8ac2de447da9c372d989e6d5c68a
GiuliaRosignoli <giuliarosignoli@gmail.com>
2021-05-11 15:47:55 +0200

    .

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="film-container flex"> 
         <ul class="single-film flex" v-for="(film, index) in filmsList" v-bind:key="index">
-            <li>{{ film.title }}</li>
-            <li v-show="film.original_title !== film.title">{{ film.original_title }}</li>
-            <li v-if="film.original_language === 'it'">{{ film.original_language }} <img class="flag" src="../assets/flagsImg/it.png" alt=""> </li>
-            <li v-else-if="film.original_language === 'en'">{{ film.original_language }} <img class="flag" src="../assets/flagsImg/en.png" alt=""></li>
-            <li v-else>{{ film.original_language }}</li>
+            <li>Title: {{ film.title }}</li>
+            <li v-show="film.original_title !== film.title">Original Title: {{ film.original_title }}</li>
+            <li v-if="film.original_language === 'it'">Language: {{ film.original_language }} <img class="flag" src="../assets/flagsImg/it.png" alt=""> </li>
+            <li v-else-if="film.original_language === 'en'">Language: {{ film.original_language }} <img class="flag" src="../assets/flagsImg/en.png" alt=""></li>
+            <li v-else>Language: {{ film.original_language }}</li>
             <li>{{ film.vote_average }}</li>
         </ul>
     </div>
