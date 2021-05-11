d3c9e7b1e68f0280e6133c934fcb9e3eec30ca6e
GiuliaRosignoli <giuliarosignoli@gmail.com>
2021-05-11 08:10:58 +0200

    .

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,7 +1,7 @@
 <template>
-    <div>
+    <div class="flex">
     
-        <ul v-for="(film, index) in filmsList" v-bind:key="index">
+        <ul class="film-list flex" v-for="(film, index) in filmsList" v-bind:key="index">
             <li>{{ film.title}}</li>
             <li>{{ film.original_title}}</li>
             <li>{{ film.original_language}}</li>
@@ -23,5 +23,16 @@ export default {
 @import "../styles/general.scss"; 
 @import "../styles/vars.scss";
 @import "../styles/utilities.scss";
+
+
+.film-list {
+    width: 100%;
+    flex-direction: column;
+    li {
+        justify-items: center;
+        flex-basis: calc(100% / 6 - 10px);
+        margin-bottom: 10px;
+    }
+}
 
 </style>
